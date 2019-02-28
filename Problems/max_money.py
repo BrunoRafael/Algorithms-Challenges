d070d80d7fd43ea6f64efdca3ed82689021ca4a0
BrunoRafael <bruno.rafael.vasconcelos@ccc.ufcg.edu.br>
2019-02-28 01:02:46 -0300

    Improvement in max_money's algorithm

--- Problems/max_money.py
+++ Problems/max_money.py
@@ -3,17 +3,15 @@ def maxMoney(n, k):
 	sum_money_students = ((n+1)*n)/2
 	money_sum = 0
 	bigger_sum = 0
-	for i in range(1, n + 1):
-		#remove actual value and sum values
-		subtract_actual_students = (sum_money_students - i)
-		if(subtract_actual_students > bigger_sum):
-			bigger_sum = subtract_actual_students
-	        
+	for i in range(1, n + 1):        
 		# Sum actual value (i) with money_sum and verify if sum result is bigger than unlucky number(k)
 		money_sum += i
 		if(money_sum == k):
 			# If money_sum is equals to unlucky value, so julia starts the remove students value process
-			return bigger_sum
+			# Julia aways removes the first student value. 
+			# In this way you will always have the highest value possible after removal because the 
+			# first one is the one that donates the least money
+			return (sum_money_students - 1)
 		elif(money_sum > k):
 			# If money_sum is bigger than k, else julia did not see unlucky number in no time
 			return sum_money_students
